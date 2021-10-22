a2e7a921a230c496013f14b15b4aaeb06c95d007
kinght-code <mdkinghtking@gmail.com>
2021-10-22 10:32:19 -0400

    My First Website

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
-      function openNav() {
+function openNav() {
         document.getElementById("mySidebar").style.width = "250px";
         document.getElementById("main").style.marginLeft = "250px";
-      }
-      
-      function closeNav() {
-        document.getElementById("mySidebar").style.width = "0";
-        document.getElementById("main").style.marginLeft= "0";
-      }
-      
-
-{/* <p>Click on the Menu Icon to transform it to "X":</p> */}
-
-
-
-
-
-
-function myFunction(x) {
-  x.classList.toggle("change");
-}
+    }
+    function closeNav() {
+          document.getElementById("mySidebar").style.width = "0";
+          document.getElementById("main").style.marginLeft= "0";
+    }

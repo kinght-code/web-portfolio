24db57d1e60a5f6cb70ae92e9b833d1abfcbcb61
kinght-code <mdkinghtking@gmail.com>
2021-10-27 09:32:47 -0400

    My First Website

--- contact.php
+++ contact.php
@@ -1,9 +1,45 @@
 <?php
-	// $name = $_POST['name'];
-    // $email = $_POST['email'];
-    // $subject = $_POST['subject'];
-    // $message = $_POST['message'];
-    
+	$name = $_POST['First Name'];
+	$lastname = $_POST['Last Name'];
+    $email = $_POST['Email'];
+    $subject = $_POST['Subject'];
+    $message = $_POST['message'];
+  
+    $to = "shahariarkazi80352@gmail.com"
+    $body = ""
+
+
+
+    $body .= "From: ".$username." \r\n";
+    $body .= "From: ".$userlastname." \r\n";
+    $body .= "From: ".$useremail." \r\n";
+    $body .= "From: ".$usersubject." \r\n";
+    $body .= "From: ".$usermassage." \r\n";
+
+    mail($to,$name,$email,$subject,$message,$body)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     // $mailheader = "From:".$name."<".$email.">\r\n";
     
     // $recipient = "sofiullahc@gmail.com";
